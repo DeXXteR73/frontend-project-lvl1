4fc7cf96460548fbadd07e2a635b4879dfc48756
dexxter <a.anf@list.ru>
2022-10-09 01:16:14 +0400

    some fixes

--- src/games/prime.js
+++ src/games/prime.js
@@ -2,13 +2,13 @@ import startGame from '../index.js';
 import random from '../utils.js';
 
 function isPrime(num) {
-  if (num <= 1) return false;
+  if (num <= 1) return 'no';
 
   for (let i = 2; i <= Math.sqrt(num); i += 1) {
-    if (Number(num % i) === 0) return false;
+    if (Number(num % i) === 0) return 'no';
   }
 
-  return true;
+  return 'yes';
 }
 
 export default function startGamePrime() {
@@ -19,9 +19,9 @@ export default function startGamePrime() {
   const num3 = random(1, 100);
   const questions = [num1, num2, num3];
 
-  const rAnswer1 = isPrime(num1) ? 'yes' : 'no';
-  const rAnswer2 = isPrime(num2) ? 'yes' : 'no';
-  const rAnswer3 = isPrime(num3) ? 'yes' : 'no';
+  const rAnswer1 = isPrime(num1);
+  const rAnswer2 = isPrime(num2);
+  const rAnswer3 = isPrime(num3);
   const rightAnswers = [rAnswer1, rAnswer2, rAnswer3];
 
   startGame(rules, questions, rightAnswers);
